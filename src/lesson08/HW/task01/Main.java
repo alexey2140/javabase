dad194bfaaf8bb36a14bd223b68d0fc5616e6513
alexey2140 <alexqac60m@gmail.com>
2016-06-13 16:16:53 +0300

    folder lesson 6 and 8 update

---
+++ src/lesson08/HW/task01/Main.java
@@ -0,0 +1,7 @@
+package lesson08.HW.task01;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/lesson08/HW/task02/Main.java
@@ -0,0 +1,7 @@
+package lesson08.HW.task02;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
