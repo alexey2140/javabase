b89f00b170ae99b8ad94ccb846f12865ae0396d6
alexey2140 <alexqac60m@gmail.com>
2016-07-05 18:41:00 +0300

    HW7 update

--- src/lesson07_HOME_TASK_02_HERE/HW7_HOME_TASK_02_HERE/task10/Main.java
+++ src/lesson07_HOME_TASK_02_HERE/HW7_HOME_TASK_02_HERE/task10/Main.java
@@ -4,8 +4,36 @@
 */
 package lesson07_HOME_TASK_02_HERE.HW7_HOME_TASK_02_HERE.task10;
 
-public class Main { // in progress
+public class Main {
     public static void main(String[] args) {
+        int[] rod1 = new int[5];
+        int[] rod2 = new int[5];
+        int[] rod3 = new int[5];
 
+        fill(rod1);             // нанизываем 5 колец на 1й стержень
+        move(rod1, rod2, 4);    // переносим кольца с 1го стержня на второй
+        move(rod2, rod3, 4);    // переносим кольца с 2го стержня на третий
+        print(rod3);            // выводим на экран содержимое 3го стержня
+    }
+
+    // в консоли пирамида перевернута
+    public static void print(int[] a) {
+        System.out.println("=================================================");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Кольцо номер: " + i + " его величина: " + a[i]);
+        }
+    }
+
+    public static void move(int[] src, int[] dist, int n) {
+        dist[4 - n] = src[n];
+        if (n > 0) {
+            move(src, dist, n - 1);
+        }
+    }
+
+    public static void fill(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = 5 - i;
+        }
     }
 }
